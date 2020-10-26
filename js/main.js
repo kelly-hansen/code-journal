8f16a07c2d6a131ba23645b6ce0e23fe807c9392
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-26 16:03:58 -0700

    On form submit, assign and reset values, reset avatar preview image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-var avatarUrlInput = document.querySelector('#avatarUrl');
+var form = document.querySelector('form');
 var avatarImg = document.querySelector('img');
+var avatarUrlInput = document.querySelector('#avatarUrl');
+var usernameInput = document.querySelector('#username');
+var fullNameInput = document.querySelector('#fullName');
+var locationInput = document.querySelector('#location');
+var bioTextarea = document.querySelector('#bio');
 
 function updateAvatar(event) {
   if (avatarUrlInput.value === '') {
@@ -10,3 +15,19 @@ function updateAvatar(event) {
 }
 
 avatarUrlInput.addEventListener('input', updateAvatar);
+
+function submit(event) {
+  avatarImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  data.profile.avatarUrl = avatarUrlInput.value;
+  avatarUrlInput.value = '';
+  data.profile.username = usernameInput.value;
+  usernameInput.value = '';
+  data.profile.fullName = fullNameInput.value;
+  fullNameInput.value = '';
+  data.profile.location = locationInput.value;
+  locationInput.value = '';
+  data.profile.bio = bioTextarea.value;
+  bioTextarea.value = '';
+}
+
+form.addEventListener('submit', submit);
