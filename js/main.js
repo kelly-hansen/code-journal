e1f80c17ddf4f276961d5dd424244d5c06d14794
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-26 16:49:58 -0700

    Added $ to DOM variable names. User data persisted to local storage

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
-var form = document.querySelector('form');
-var avatarImg = document.querySelector('img');
-var avatarUrlInput = document.querySelector('#avatarUrl');
-var usernameInput = document.querySelector('#username');
-var fullNameInput = document.querySelector('#fullName');
-var locationInput = document.querySelector('#location');
-var bioTextarea = document.querySelector('#bio');
+var $form = document.querySelector('form');
+var $avatarImg = document.querySelector('img');
+var $avatarUrlInput = document.querySelector('#avatarUrl');
+var $usernameInput = document.querySelector('#username');
+var $fullNameInput = document.querySelector('#fullName');
+var $locationInput = document.querySelector('#location');
+var $bioTextarea = document.querySelector('#bio');
 
 function updateAvatar(event) {
-  if (avatarUrlInput.value === '') {
-    avatarImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  if ($avatarUrlInput.value === '') {
+    $avatarImg.setAttribute('src', 'images/placeholder-image-square.jpg');
   } else {
-    avatarImg.setAttribute('src', event.target.value);
+    $avatarImg.setAttribute('src', event.target.value);
   }
 }
 
-avatarUrlInput.addEventListener('input', updateAvatar);
+$avatarUrlInput.addEventListener('input', updateAvatar);
 
 function submit(event) {
-  avatarImg.setAttribute('src', 'images/placeholder-image-square.jpg');
-  data.profile.avatarUrl = avatarUrlInput.value;
-  avatarUrlInput.value = '';
-  data.profile.username = usernameInput.value;
-  usernameInput.value = '';
-  data.profile.fullName = fullNameInput.value;
-  fullNameInput.value = '';
-  data.profile.location = locationInput.value;
-  locationInput.value = '';
-  data.profile.bio = bioTextarea.value;
-  bioTextarea.value = '';
+  event.preventDefault();
+  $avatarImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  data.profile.avatarUrl = $avatarUrlInput.value;
+  $avatarUrlInput.value = '';
+  data.profile.username = $usernameInput.value;
+  $usernameInput.value = '';
+  data.profile.fullName = $fullNameInput.value;
+  $fullNameInput.value = '';
+  data.profile.location = $locationInput.value;
+  $locationInput.value = '';
+  data.profile.bio = $bioTextarea.value;
+  $bioTextarea.value = '';
+  for (var key in data.profile) {
+    localStorage.setItem(key, data.profile[key]);
+  }
 }
 
-form.addEventListener('submit', submit);
+$form.addEventListener('submit', submit);
